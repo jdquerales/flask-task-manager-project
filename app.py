53df3575fe5b0b9e73e8969392a0ed49af517f30
jdquerales <jquerales72@gmail.com>
2021-01-07 22:02:57 +0000

    Set secret_key

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import os
 from flask import (
     Flask, flash, render_template,
     redirect, request, session, url_for)
+from flask_session import Session
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -15,6 +16,7 @@ app = Flask(__name__)
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
+sess.init_app(app)
 
 
 mongo = PyMongo(app)
@@ -203,4 +205,4 @@ def delete_category(category_id):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=False)
+            debug=True)
