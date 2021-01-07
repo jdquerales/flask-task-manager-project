cd27828621b2d8f547d679bea9656f5ea2c8ab3c
jdquerales <jquerales72@gmail.com>
2021-01-07 23:23:17 +0000

    Correct secret_key

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import os
 from flask import (
     Flask, flash, render_template,
     redirect, request, session, url_for)
-from flask_session import Session
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -12,11 +11,10 @@ if os.path.exists("env.py"):
 
 app = Flask(__name__)
 
-
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
-sess.init_app(app)
+
 
 
 mongo = PyMongo(app)
